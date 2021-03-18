14bd31e4f4c9361329dff918480f1357c194f680
mdiakhate12 <mdiakhate1297@gmail.com>
2021-03-18 21:13:49 +0000

    Added SBAM Stack

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 terraform-apply:
 	cd terraform/ && \
-	sed -e "s/RESOURCE_NAME/${RESOURCE_NAME}/" template > ${RESOURCE_NAME}.tf && \
-	 echo "ressource name: ${RESOURCE_NAME}" && \
+	 mv main.tf ${RESOURCE_NAME}.tf && \
 	 echo "Created file: ${RESOURCE_NAME}.tf from template..." && \
 	 terraform init && \
 	 terraform plan -out ${RESOURCE_NAME}.plan && \
--- index.js
+++ index.js
@@ -1,193 +1,238 @@
-const express = require('express');
-const cors = require('cors');
-const fs = require('fs')
-const Compute = require('@google-cloud/compute');
-const { Storage } = require('@google-cloud/storage');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const Compute = require("@google-cloud/compute");
+const { Storage } = require("@google-cloud/storage");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 const WAIT_BEFORE_EXECUTE = 3000; // wait 6 seconds
 
-const app = express()
+const app = express();
 
-const templateRepository = 'template_repository';
-const templateRegistry = 'template_registry';
+const templateRepository = "template_repository";
+const templateRegistry = "template_registry";
 
-const files = [
-    'terraform/google/mern/dev/template',
-    'terraform/google/mern/dev/variables.tf',
-    'modules/react/dev/init-react.sh',
-    'modules/nodejs/dev/init-nodejs.sh',
-    'modules/mongodb/init-mongodb.sh',
-]
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+const getName = (str) => str.split("/")[1];
+const getPath = (str) => str.substring(str.lastIndexOf("/") + 1);
 
 app.post("/", async (req, res) => {
+  try {
+    const { instanceGroupName, stack } = req.body;
 
-    try {
+    let files = [
+      "terraform/google/dev/main.tf",
+      "terraform/google/dev/variables.tf",
+    ];
 
-        const {
-            instanceGroupName,
-            projectName,
-        } = req.body
+    let modules = [];
 
-        // Replace hyphens by underscores
-        let resourceName = instanceGroupName.replace(/-/g, '_').trim().toLowerCase()
-        // let folders = "backend"
-        let timestamp = Date.now()
+    switch (stack) {
+      case "mern":
+        modules = {
+          frontend: "modules/react/dev/init-react.sh",
+          backend: "modules/nodejs/dev/init-nodejs.sh",
+          database: "modules/mongodb/init-mongodb.sh",
+        };
+        break;
 
-        // Download the files
-        downloadFiles(files, "./terraform/")
+      case "sbam":
+        modules = {
+          frontend: "modules/angular/dev/init-angular.sh",
+          backend: "modules/springboot/dev/init-spring-boot.sh",
+          database: "modules/mysql/init-mysql.sh",
+        };
+        break;
 
-
-        // Wait 3 seconds for the dowload
-        setTimeout(() => {
-
-            createTerraformVariableFile(req.body, resourceName)
-
-            executeTerraform(resourceName)
-                .then(async () => {
-                    // Export the generated terraform directory to template registry
-                    uploadFiles(projectName, instanceGroupName, timestamp)
-
-                    // Get VM Instances
-                    getInstances(instanceGroupName)
-                        .then(instances => {
-                            console.log(instances)
-                            res.send(instances)
-                        }).catch(console.error)
-                })
-                .catch((error) => { console.error(error); res.status(500).send("Error during creation.") })
-
-        }, WAIT_BEFORE_EXECUTE);
-
-    } catch (error) {
-        console.error(error)
-        res.status(500).send("Server error.")
-
+      default:
+        break;
     }
 
-})
+    for ([key, value] of Object.entries(modules)) {
+      files.push(value);
+    }
 
-const downloadFiles = (files, path) => {
+    // Replace hyphens by underscores
+    // let resourceName = instanceGroupName
+    //   .replace(/-/g, "_")
+    //   .trim()
+    //   .toLowerCase();
+    // let folders = "backend"
+    let timestamp = Date.now();
 
-    // Creates a client
-    const storage = new Storage();
+    // Download the files
+    downloadFiles(files, "./terraform/");
 
-    files.map(async (file) => {
-        let destination = `${path}/${file.substring(file.lastIndexOf("/") + 1)}`
-        try {
-            await storage.bucket(templateRepository).file(file).download({ destination })
-            console.log(`gs://${templateRepository}/${file} downloaded to ${destination}.`)
-        }
-        catch (error) {
-            console.error(error)
-        }
-    })
-}
+    // Wait 3 seconds for the dowload
+    setTimeout(() => {
+      createTerraformVariableFile(req.body, modules);
 
-const uploadFiles = async (projectName, instanceGroupName, timestamp) => {
+      executeTerraform(instanceGroupName)
+        .then(async () => {
+          // Export the generated terraform directory to template registry
+          uploadFiles(instanceGroupName, timestamp);
 
-    const storage = new Storage();
-
-    let path = `terraform`
-
-    return fs.readdir(path, async (err, files) => {
-        if (err) {
-            console.error(err)
-            return
-        }
-
-        console.log("TIMESTAMP:", timestamp)
-
-        // Import and remove each file one by one 
-        files.forEach(file => {
-            if (!fs.statSync(`${path}/${file}`).isDirectory()) {
-                let destination = `${instanceGroupName}-${timestamp}/${file}`
-                storage
-                    .bucket(templateRegistry)
-                    .upload(`${path}/${file}`, { destination })
-                    .then(async () => {
-                        console.log(`${file} uploaded successfuly`)
-                        exec(`rm -rf ${path}/${file}`)
-                            .then((() => console.log(`Deleted ${file}.`)))
-                            .catch(console.error)
-                    })
-                    .catch(console.error)
-            }
+          // Get VM Instances
+          getInstances(instanceGroupName)
+            .then((instances) => {
+              console.log(instances);
+              res.send(instances);
+            })
+            .catch(console.error);
         })
-        exec(`rm -rf ${path}/.terraform`)
-            .then((() => console.log("Deleted .terraform plugin folder.")))
-            .catch(console.error)
-    })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send("Error during creation.");
+        });
+    }, WAIT_BEFORE_EXECUTE);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error.");
+  }
+});
 
-}
+const downloadFiles = (filesToDowload, path) => {
+  // Creates a client
+  const storage = new Storage();
+
+  filesToDowload.map(async (file) => {
+    let destination = `${path}/${file.substring(file.lastIndexOf("/") + 1)}`;
+    try {
+      await storage
+        .bucket(templateRepository)
+        .file(file)
+        .download({ destination });
+      console.log(
+        `gs://${templateRepository}/${file} downloaded to ${destination}.`
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  });
+};
+
+const uploadFiles = async (instanceGroupName, timestamp) => {
+  const storage = new Storage();
+
+  let path = `terraform`;
+
+  return fs.readdir(path, async (err, directoryContent) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log("TIMESTAMP:", timestamp);
+
+    // Import and remove each file one by one
+    directoryContent.forEach((file) => {
+      if (!fs.statSync(`${path}/${file}`).isDirectory()) {
+        let destination = `${instanceGroupName}-${timestamp}/${file}`;
+        storage
+          .bucket(templateRegistry)
+          .upload(`${path}/${file}`, { destination })
+          .then(async () => {
+            console.log(`${file} uploaded successfuly`);
+            // exec(`rm -rf ${path}/${file}`)
+            //   .then(() => console.log(`Deleted ${file}.`))
+            //   .catch(console.error);
+          })
+          .catch(console.error);
+      }
+    });
+    // exec(`rm -rf ${path}/.terraform`)
+    //   .then(() => console.log("Deleted .terraform plugin folder."))
+    //   .catch(console.error);
+  });
+};
 
 const getInstances = async (prefix) => {
-    const compute = new Compute()
-    return compute
-        .getVMs({ filter: `name eq ^${prefix}.*` })
-        .then(data => {
+  const compute = new Compute();
+  return compute
+    .getVMs({ filter: `name eq ^${prefix}.*` })
+    .then((data) => {
+      let vms = data[0].map((element) => element.metadata);
+      let newVMs = vms.map(({ name, networkInterfaces }) => ({
+        name,
+        publicIP: networkInterfaces[0].accessConfigs[0].natIP,
+        privateIP: networkInterfaces[0].networkIP,
+      }));
 
-            let vms = data[0].map(element => element.metadata)
-            let newVMs = vms.map(({ name, networkInterfaces }) => ({
-                name,
-                publicIP: networkInterfaces[0].accessConfigs[0].natIP,
-                privateIP: networkInterfaces[0].networkIP
-            }))
+      // console.log(newVMs)
 
-            // console.log(newVMs)
+      return newVMs;
+    })
+    .catch(console.error);
+};
 
-            return newVMs;
-        })
-        .catch(console.error)
-}
+const createTerraformVariableFile = async (body, modules) => {
+  const {
+    numberOfVm,
+    instanceGroupName,
+    cpu,
+    memory,
+    disk,
+    osType,
+    osImage,
+    applicationType,
+  } = body;
 
-const createTerraformVariableFile = async (body, resourceName) => {
+  let instance = {
+    number_of_vm: numberOfVm,
+    vm_group_name: instanceGroupName,
+    cpu: cpu,
+    memory: memory,
+    disk_size_gb: disk,
+    image_project: osType,
+    image_family: osImage,
+    application_type: applicationType,
+    region: "europe-west6",
+    zone: "europe-west6-a",
+  };
 
-    const { numberOfVm, instanceGroupName, cpu, memory, disk, osType, osImage, applicationType } = body
+  if (process.env.USER) {
+    instance["user"] = process.env.USER;
+  }
 
-    let instance = {
-        number_of_vm: numberOfVm,
-        vm_group_name: instanceGroupName,
-        cpu: cpu,
-        memory: memory,
-        disk_size_gb: disk,
-        image_project: osType,
-        image_family: osImage,
-        application_type: applicationType,
-    }
+  for ([key, value] of Object.entries(modules)) {
+    instance[key] = { name: getName(value), path: getPath(value) };
+  }
 
-    if (process.env.USER) {
-        instance['user'] = process.env.USER
-    }
-
-    fs.writeFileSync(`terraform/${resourceName}.auto.tfvars.json`, JSON.stringify(instance))
-    console.log(`Created file terraform/${resourceName}.auto.tfvars.json from request...`)
-}
+  fs.writeFileSync(
+    `terraform/${instanceGroupName}.auto.tfvars.json`,
+    JSON.stringify(instance)
+  );
+  console.log(
+    `Created file terraform/${instanceGroupName}.auto.tfvars.json from request...`
+  );
+};
 
 const executeTerraform = async (resourceName) => {
+  return (
+    exec(`make terraform-apply RESOURCE_NAME=${resourceName}`)
+      // When success
+      .then(async ({ stdout, stderr, error }) => {
+        if (error) {
+          console.error(error);
+          return;
+        }
 
-        return exec(`make terraform-apply RESOURCE_NAME=${resourceName}`)
-
-            // When success
-            .then(async ({ stdout, stderr, error }) => {
-                if (error) { console.error(error); return }
-
-                // Log output
-                if (stderr) console.log(`stderr: ${stderr}`)
-                if (stdout) console.log(`stdout: ${stdout}`)
-                return  
-            })
-            .catch(error => {
-                console.error(error)
-                return
-            })
-}
+        // Log output
+        if (stderr) console.log(`stderr: ${stderr}`);
+        if (stdout) console.log(`stdout: ${stdout}`);
+        return;
+      })
+      .catch((error) => {
+        console.error(error);
+        return;
+      })
+  );
+};
 
 app.listen(PORT, () => {
-    console.log('Listenning on port: ', PORT)
-})
+  console.log("Listenning on port: ", PORT);
+});
